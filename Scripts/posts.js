57de431bb683e92efba8e4dd4a496e3ed65931dd
allen <allenriath@live.com>
2021-04-01 17:28:59 -0300

    Trying to Load into #home-wraper

--- Scripts/posts.js
+++ Scripts/posts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $("#posts_link").click(function(){
-        $("#baseContainer").load("https://allenriath.github.io/kazumi-site/posts.html");
+        $("#home-wraper").load("https://allenriath.github.io/kazumi-site/posts.html");
     });
 });
