29589e6c751efdd07c9cf473b6c0154bc8725e0f
allen <allenriath@live.com>
2021-04-01 19:11:49 -0300

    Load Alert checking.

--- Scripts/posts.js
+++ Scripts/posts.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $("#posts_link").click(function(){
-        $("#home-wraper").load("https://allenriath.github.io/kazumi-site/posts.html");
+        $("#home-wraper").load("https://allenriath.github.io/kazumi-site/posts.html", function() {
+            alert("Load has been performed");
+        });
     });
 });
