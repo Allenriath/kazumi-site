e6b45544b01ad902176cc3b627f3f061ae077e8a
allen <allenriath@live.com>
2021-04-01 16:20:53 -0300

    update

--- Scripts/posts.js
+++ Scripts/posts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $("#posts_link").click(function(){
-        $("#baseContainer").load("/tagged/arts");
+        $("#baseContainer").load("https://allenriath.github.io/kazumi-site/posts.html");
     });
 });
