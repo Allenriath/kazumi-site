a07ac7b425c95a64819e50601ea950a2138c237e
allen <allenriath@live.com>
2021-04-02 15:26:02 -0300

    Put blocks back and cleaned console response

--- Scripts/posts.js
+++ Scripts/posts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#posts_link").click(function(){
         $("#test").load("https://allenriath.github.io/kazumi-site/posts.html", function( response, status, xhr) {
-               console.log (response +" "+ xhr.status +" "+ xhr.statusText);
+               console.log ("Response: "+ response +"Status: "+ xhr.status +"Status Text "+ xhr.statusText);
         });
     });
 });
