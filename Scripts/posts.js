7440ea4bb9e01c4eab59ba132dd8db8bd0016feb
allen <allenriath@live.com>
2021-04-02 03:04:04 -0300

    Trying to check console for error messages.

--- Scripts/posts.js
+++ Scripts/posts.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
     $("#posts_link").click(function(){
-        $("#test").load("https://allenriath.github.io/kazumi-site/posts.html", function() {
-            alert("Load has been performed");
+        $("#test").load("https://allenriath.github.io/kazumi-site/posts.html", function( response, status, xhr) {
+            if ( status == "error") {
+                var msg = "Error while loading file: ";
+                console.log( msg + xhr.status + " " + xhr.statusText);
+            }
         });
     });
 });
