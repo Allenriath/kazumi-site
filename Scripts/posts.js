987c72d7eff1cd979a8d02dff2cbe23fd6d425c3
allen <allenriath@live.com>
2021-04-26 08:07:58 -0300

    removed posts ajax files

--- Scripts/posts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#posts_link").click(function(){
-        $("#test").load("https://allenriath.github.io/kazumi-site/posts.html", function( response, status, xhr) {
-               console.log ("Response: "+ response +"Status: "+ xhr.status +"Status Text "+ xhr.statusText);
-        });
-    });
-});
